52beb838d0f7f008f16f337030a4c86df850eae7
Artem Romanko <rinart73@gmail.com>
2019-06-25 03:24:04 +0200

    1.0.4 * Updated to 0.23.3 * Now it's a serverside mod

--- data/scripts/entity/story/adventurer1.lua
+++ data/scripts/entity/story/adventurer1.lua
@@ -1,40 +1,17 @@
-local adventurerFixes_updateClient, adventurerFixes_onMeetAdventurer
-
-if onClient() then
+local adventurerFixes_givePlayerGoodie
 
 
-adventurerFixes_updateClient = updateClient
-function updateClient(...)
-    if Player():getValue("talked_to_adventurer") then return end
-
-    adventurerFixes_updateClient(...)
-end
-
-function initUI() -- overridden
-    ScriptUI():registerInteraction("Greetings"%_t, "onGreet") -- fix localization
-end
+if onServer() then
 
 
-else -- onServer
-
-
-function givePlayerGoodie() -- overridden
-    -- keep this part in case mod was installed on pre-existing server
-    if data.given[callingPlayer] then return end
+adventurerFixes_givePlayerGoodie = Adventurer1.givePlayerGoodie
+function Adventurer1.givePlayerGoodie(...)
     local player = Player(callingPlayer)
     if player:getValue("got_adventurer_system") then return end
     
-    data.given[callingPlayer] = true
     player:setValue("got_adventurer_system", true)
     
-    player:getInventory():add(SystemUpgradeTemplate("data/scripts/systems/radarbooster.lua", Rarity(1), Seed(124)))
-end
-
-adventurerFixes_onMeetAdventurer = onMeetAdventurer
-function onMeetAdventurer(...)
-    Player(callingPlayer):setValue("talked_to_adventurer", true)
-
-    adventurerFixes_onMeetAdventurer(...)
+    adventurerFixes_givePlayerGoodie(...)
 end
 
 

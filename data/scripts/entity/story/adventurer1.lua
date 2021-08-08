f679e6cf0e3c8e6e5be254dae94c91b6b2bfa1d6
Rinart73 <rinart73@gmail.com>
2021-08-08 06:20:17 +0200

    1.0.9 * Updated to 2.0

--- data/scripts/entity/story/adventurer1.lua
+++ data/scripts/entity/story/adventurer1.lua
@@ -1,17 +1,17 @@
-local adventurerFixes_givePlayerGoodie
+local adf_givePlayerGoodie -- extended server function
 
 
 if onServer() then
 
 
-adventurerFixes_givePlayerGoodie = Adventurer1.givePlayerGoodie
+adf_givePlayerGoodie = Adventurer1.givePlayerGoodie
 function Adventurer1.givePlayerGoodie(...)
     local player = Player(callingPlayer)
     if player:getValue("got_adventurer_system") then return end
     
     player:setValue("got_adventurer_system", true)
     
-    adventurerFixes_givePlayerGoodie(...)
+    adf_givePlayerGoodie(...)
 end
 
 
